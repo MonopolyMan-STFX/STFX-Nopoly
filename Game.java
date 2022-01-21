c320db6556e14b07f60310987d67e5543b3ac32d
CP2021 <98051805+Craig2022@users.noreply.github.com>
2022-01-21 16:39:01 +0000

    Created dice added check for double

--- Game.java
+++ Game.java
@@ -1,13 +1,67 @@
+import java.util.*;
+
 /**
  * This is the main game class
  *
- * @author JM
+ * @author Aaron, Craig, Kyle, Basel
  */
 
-
 class Game {
+
+    // Attributes
+	private ArrayList<String> board = new ArrayList<String>();
+    private ArrayList<String> players = new ArrayList<String>();
+	private int[] tileInGame;
+    private int dice1 = 0;
+    private int dice2 = 0;
+
+	// Constructor
+    public Game() {
+		// Set data as attributes
+    } 
+    
+    /**
+     *  toString function
+     */
+ 	public String toString( ) {
+ 	   String result = "";
+ 		return result;
+   }
+  
+    /*
+    * Roll Die
+    * @return total
+    */
+    public int rollDie(){
+        Random rand = new Random();
+        // Randomize value of two dice
+        dice1 = rand.nextInt(6)+1;
+        dice2 = rand.nextInt(6)+1;
+        // Calculate and return total
+        int total = dice1 + dice2;
+        return total;
+    }
+
+    /*
+    * Check if rolled doubles
+    * @return if double
+    */
+    public boolean checkDouble(){
+        boolean isDouble = false;
+        if (dice1 == dice2) {
+            isDouble = true;
+        }
+        return isDouble;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello");
+
+        Game game = new Game();
+
+        // Test roll
+        System.out.println("Roll: "+game.rollDie());
+        System.out.println("Double: "+game.checkDouble());
+
         Square test = new Square(12, "test", "test");
         System.out.println(test.toString());
         System.out.println("\n");
@@ -16,7 +70,7 @@ class Game {
         int[] rent = {50,200, 600, 1400, 1700, 2000};
 
         // https://monopoly.fandom.com/wiki/Boardwalk
-        //
+        
         // Board walk property
         Property boardWalk = new Property(4, "Board Walk", 400, rent, 0, false, 200, 200, "blue" );
 
--- Player.java
+++ Player.java
@@ -6,145 +6,125 @@ import java.util.*;
  * @Course ICS4UC
  */
 
-// CTRL + / to uncomment
-
 class Player {
-    // Attributes
-    private String playerToken = "";
-    private int money = 0;
+  // Attributes
+  private String playerToken = "";
+  private int money = 0;
 
-    // USE THIS
-    // private ArrayList<Property> propertys = new ArrayList<Property>();
-    
-    
-    // To stop errors, until property is finished
-    private ArrayList<Integer> propertys = new ArrayList<Integer>();
+  // USE THIS
+  // private ArrayList<Property> propertys = new ArrayList<Property>();
 
+  // To stop errors, until property is finished
+  private ArrayList<Integer> propertys = new ArrayList<Integer>();
 
+  private int position = 0;
+  private boolean inJail = false;
+  private short numDoubles = 0;
 
+  /**
+   * deposit money into the account
+   * 
+   * @param moneyIn
+   */
+  public void deposit(int moneyIn) {
+    money += moneyIn;
+  }
 
-
-    private int position = 0;
-    private boolean inJail = false;
-    private short numDoubles = 0;
-
-    /**
-     * deposit money into the account
-     * @param moneyIn
-     */
-    public void deposit(int moneyIn) 
-    {
-        money += moneyIn;
+  /**
+   * Withdraws money, if false it failed
+   * 
+   * @param moneyOut
+   * @return result
+   */
+  public boolean withdraw(int moneyOut) {
+    boolean result;
+    if (moneyOut > money) {
+      result = false;
+    } else {
+      result = true;
+      money -= moneyOut;
     }
 
-    /**
-     * Withdraws money, if false it failed
-     * @param moneyOut
-     * @return result
-     */
-    public boolean withdraw(int moneyOut)
-    {
-        boolean result;
-        if(moneyOut > money)
-        {
-            result = false;
-        }
-        else
-        {
-            result = true;
-            money -= moneyOut;
-        }
+    return result;
+  }
 
-        return result;
+  /**
+   * Returns money
+   * 
+   * @return
+   */
+  public int getBalance() {
+    return money;
+  }
+
+  /**
+   * Moves directly to that tile
+   * 
+   * @param moved
+   */
+  public void changePositionDirect(int newTile) {
+    position = newTile;
+  }
+
+  /**
+   * Moves up
+   * 
+   * @param moveUp
+   */
+  public void moveUp(int moveUp) {
+    // Checks if it passed go, and adds funds accordingly
+    if ((position + moveUp) > 40) {
+      money += 200;
+      position = (position + moveUp) - 40;
+    } else {
+      position += moveUp;
+    }
+  }
+
+  /**
+   * Adds the index of a certain property to the ArrayList
+   * 
+   * @param indexOfProperty
+   */
+  public void addProperty(int indexOfProperty) {
+    // propertys.add(indexOfProperty);
+  }
+
+  /**
+   * Searches for the property in a linear fashion, if not found it returns false
+   * 
+   * @param indexOfProperty
+   * @return found
+   */
+  public boolean removeProperty(int indexOfProperty) {
+    // Search through the array in a linear fashion, and remove the value
+    boolean found = false;
+    int i = 0;
+    while (found == false) {
+
+      // When found, stop searching
+      if (propertys.get(i) == indexOfProperty) {
+        found = true;
+      } else {
+        i++;
+      }
+
+      // If not found, exit and give up
+      if (i == propertys.size()) {
+        found = true;
+        i = -1;
+      }
     }
 
-    /**
-     * Returns money
-     * @return
-     */
-    public int getBalance()
-    {
-        return money;
+    // Checks if it was not found by the end, and sets found to false to be returned
+    if (i == -1) {
+      found = false;
+    }
+    // Otherwise remove that property
+    else {
+      propertys.remove(i);
     }
 
-    /**
-     * Moves directly to that tile
-     * @param moved
-     */
-    public void changePositionDirect(int newTile)
-    {
-        position = newTile;
-    }
-
-    /**
-     * Moves up
-     * @param moveUp
-     */
-    public void moveUp(int moveUp)
-    {
-        // Checks if it passed go, and adds funds accordingly
-        if((position + moveUp) > 40)
-        {
-            money += 200;
-            position = (position + moveUp) - 40;
-        }
-        else
-        {
-            position += moveUp;
-        }
-    }
-
-    /**
-     * Adds the index of a certain property to the ArrayList
-     * @param indexOfProperty
-     * */
-    public void addProperty(int indexOfProperty)
-    {
-        // propertys.add(indexOfProperty);
-    }
-
-    /**
-     * Searches for the property in a linear fashion, if not found it returns false
-     * @param indexOfProperty
-     * @return found
-     * */
-    public boolean removeProperty(int indexOfProperty)
-    {
-                // Search through the array in a linear fashion, and remove the value
-                boolean found = false;
-                int i = 0;
-        while(found == false)
-                {
-
-                        // When found, stop searching
-                        if(propertys.get(i) == indexOfProperty)
-                        {
-                                found = true;
-                        }
-                        else
-                        {
-                                i++;
-                        }
-
-                        // If not found, exit and give up
-                        if(i == propertys.size())
-                        {
-                                found = true;
-                                i = -1;
-                        }
-                }
-
-        // Checks if it was not found by the end, and sets found to false to be returned
-                if(i == -1)
-                {
-                        found = false;
-                }
-        // Otherwise remove that property
-                else
-                {
-                        propertys.remove(i);
-                }
-
-                return found;
-    }
+    return found;
+  }
 }
